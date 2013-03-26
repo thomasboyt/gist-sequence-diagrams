e196f632db38b6f5bbcdfa31c1c8412b611d05c8
Thomas Boyt <me@thomasboyt.com>
2013-03-26 18:21:45 -0400

    auto-update diagram

--- static/javascripts/templates.js
+++ static/javascripts/templates.js
@@ -5,44 +5,36 @@ this["JST"] = this["JST"] || {};
 this["JST"]["static/templates/diagram.handlebars"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [2,'>= 1.0.0-rc.3'];
 helpers = helpers || Handlebars.helpers; data = data || {};
-  var buffer = "", stack1, stack2, functionType="function", escapeExpression=this.escapeExpression, self=this;
+  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression;
 
-function program1(depth0,data) {
-  
-  var buffer = "", stack1;
-  buffer += "\n    ";
-  stack1 = helpers['with'].call(depth0, depth0.attributes, {hash:{},inverse:self.noop,fn:self.program(2, program2, data),data:data});
-  if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "\n  ";
-  return buffer;
-  }
-function program2(depth0,data) {
-  
-  var buffer = "", stack1;
-  buffer += "\n    <div class=\"file\">\n      <strong>";
+
+  buffer += "<div class=\"file\">\n  <strong>";
   if (stack1 = helpers.filename) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.filename; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
-    + "</strong>\n      <div class=\"diagram\">\n        ";
+    + "</strong>\n  <div class=\"diagram\">\n    ";
   if (stack1 = helpers.content) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.content; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
-    + "\n      </div>\n      <div class=\"editor-wrapper\">\n        <div class=\"editable\">";
+    + "\n  </div>\n  <div class=\"editor-wrapper\">\n    <div class=\"editable\">";
   if (stack1 = helpers.content) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.content; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
-    + "</div>\n      </div>\n    </div>\n    ";
+    + "</div>\n  </div>\n</div>\n";
   return buffer;
-  }
+  });
+
+this["JST"]["static/templates/gist.handlebars"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+helpers = helpers || Handlebars.helpers; data = data || {};
+  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression;
+
 
   buffer += "<h2>";
   if (stack1 = helpers.description) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.description; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
-    + "</h2>\n\n<div>\n  ";
-  stack2 = helpers.each.call(depth0, ((stack1 = depth0.files),stack1 == null || stack1 === false ? stack1 : stack1.models), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
-  if(stack2 || stack2 === 0) { buffer += stack2; }
-  buffer += "\n</div>\n";
+    + "</h2>\n\n<div class=\"files\">\n</div>\n";
   return buffer;
   });
 
--- static/javascripts/views/diagram_view.js
+++ static/javascripts/views/diagram_view.js
@@ -1,13 +1,28 @@
 define(['templates'], function(templates) {
-  var template = templates['static/templates/diagram.handlebars'];
+  var gistTemplate = templates['static/templates/gist.handlebars'];
+  var diagramTemplate = templates['static/templates/diagram.handlebars'];
+
+  // todo: split out individual files to some sort of FileView
   var DiagramView = Backbone.View.extend({
     render: function() {
-      this.$el.html(template(_.extend({}, this.model.attributes)));
-      this.$(".diagram").sequenceDiagram({theme: 'hand'});
-      $(".editable").each(function() {
-        var editor = ace.edit(this);
+      this.$el.html(gistTemplate(this.model.attributes));
+
+      this.model.get("files").each(function(file) {
+        var html = diagramTemplate(file.attributes);
+        var $el = $(html).appendTo(this.$(".files"));
+        var diagramDiv = $(".diagram", $el);
+        diagramDiv.sequenceDiagram({theme: 'hand'});
+
+        var editor = ace.edit($(".editable", $el)[0]);
         editor.setTheme("ace/theme/monokai");
-        editor.getSession().setMode("ace/mode/text");
+
+        editor.getSession().on('change', _.debounce(function(e) {
+          var newValue = editor.getValue();
+          var diagram = Diagram.parse(newValue);
+          diagramDiv.html("");
+          diagram.drawSVG(diagramDiv.get(0));
+          file.set("content", newValue);
+        }, 350));
       });
       return this;
     }
